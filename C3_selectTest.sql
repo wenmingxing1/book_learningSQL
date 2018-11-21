5b93e5521d008a968c4378382b6cad9d4ad5e6c8
wenmingxing1 <1793701192@qq.com>
2018-11-21 16:41:35 +0800

    Update C3_selectTest.sql
    select

--- C3_selectTest.sql
+++ C3_selectTest.sql
@@ -1,49 +1,118 @@
 ## 第3章 查询入门
+
 show databases;
 use test;
 show tables;
 select * from department;
 
-select emp_id, 'ACTIVE', emp_id*3.14, upper(lname) from employee;
+SELECT 
+    emp_id, 'ACTIVE', emp_id * 3.14, UPPER(lname)
+FROM
+    employee;
 
 select version(), user(), database();
 
-select emp_id, 'ACTIVE' as status, emp_id*3.14 as empid_x_pi, upper(lname) as last_name_upper from employee;
+SELECT 
+    emp_id,
+    'ACTIVE' AS status,
+    emp_id * 3.14 AS empid_x_pi,
+    UPPER(lname) AS last_name_upper
+FROM
+    employee;
 
 select distinct cust_id from account;
 
-select e.emp_id, e.fname, e.lname from (select emp_id, fname, lname, start_date, title from employee) e;
+SELECT 
+    e.emp_id, e.fname, e.lname
+FROM
+    (SELECT 
+        emp_id, fname, lname, start_date, title
+    FROM
+        employee) e;
 
-create view employee_vw as select emp_id, fname, lname, year(start_date) start_year from employee;	##创建视图
+CREATE VIEW employee_vw AS
+    SELECT 
+        emp_id, fname, lname, YEAR(start_date) start_year
+    FROM
+        employee;	##创建视图
 
-select emp_id, start_year from employee_vw;
+SELECT 
+    emp_id, start_year
+FROM
+    employee_vw;
 
-select emp_id, fname, lname, start_date, title from employee where title = 'Head Teller';
+SELECT 
+    emp_id, fname, lname, start_date, title
+FROM
+    employee
+WHERE
+    title = 'Head Teller';
 
-select emp_id, fname, lname, start_date, title from employee where title = 'Head Teller' or start_date > '2006-01-01';
+SELECT 
+    emp_id, fname, lname, start_date, title
+FROM
+    employee
+WHERE
+    title = 'Head Teller'
+        OR start_date > '2006-01-01';
 
-select d.name, count(e.emp_id) num_employees from department as d inner join employee as e on d.dept_id = e.dept_id 
-group by d.name having count(e.emp_id)>2;
+SELECT 
+    d.name, COUNT(e.emp_id) num_employees
+FROM
+    department AS d
+        INNER JOIN
+    employee AS e ON d.dept_id = e.dept_id
+GROUP BY d.name
+HAVING COUNT(e.emp_id) > 2;
 
-select distinct open_emp_id, product_cd from account order by open_emp_id desc limit 0,5;
+SELECT DISTINCT
+    open_emp_id, product_cd
+FROM
+    account
+ORDER BY open_emp_id DESC
+LIMIT 0 , 5;
 
-select cust_id, cust_type_cd, city, state, fed_id from customer order by right(fed_id, 4);
+SELECT 
+    cust_id, cust_type_cd, city, state, fed_id
+FROM
+    customer
+ORDER BY RIGHT(fed_id, 4);
 
 
 ############################################################# test
 
 ## test3-1
-select emp_id, fname, lname from employee order by lname, fname;
+SELECT 
+    emp_id, fname, lname
+FROM
+    employee
+ORDER BY lname , fname;
 
 ## test3-2
-select account_id, cust_id, avail_balance from account where status = 'ACTIVE' and avail_balance > 2500;
+SELECT 
+    account_id, cust_id, avail_balance
+FROM
+    account
+WHERE
+    status = 'ACTIVE'
+        AND avail_balance > 2500;
 
 ## test3-3
-select distinct open_emp_id from account;
+SELECT DISTINCT
+    open_emp_id
+FROM
+    account;
 
 ## test3-4
-select p.product_cd, a.cust_id, a.avail_balance from product p inner join account a on p.product_cd = a.product_cd
-where p.product_type_cd = 'ACCOUNT' order by p.product_cd, a.cust_id;
+SELECT 
+    p.product_cd, a.cust_id, a.avail_balance
+FROM
+    product p
+        INNER JOIN
+    account a ON p.product_cd = a.product_cd
+WHERE
+    p.product_type_cd = 'ACCOUNT'
+ORDER BY p.product_cd , a.cust_id;
 
 
 
